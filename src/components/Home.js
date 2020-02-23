6eb7e40e93f414c4d2d39c5f4e680ec1b9740eaa
DwayneL <dr2@ameritech.net>
2020-02-23 17:45:06 -0600

    updated profile page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React from "react";
 
-function Home (){
-    return(
-        <h1>Home goes here</h1>
-    )
+function Home() {
+  return (
+    <p class="col-md-8">
+      Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in the
+      neighborhood whre I grew up on the Westside. I attended Loyola University
+      Chicago, graduating in summer of 2011 with a BA in Music. Since graduation
+      I have spent time working as a music teacher, performing pianist, composer
+      and producer. For the last 2 years I have also been the current lead for
+      the account management/collections team for zZounds.com, a leading company
+      in the instrument retail business. Part of my day to day job is
+      overlooking some of the coding of our internal wiki page and doing some
+      light coding in HTML.
+      <br />
+      <br />
+      I am currently enrolled in the Coding Bootcamp at Northwestern University
+      with a scheduled completion date of March 2020. I hope to gain a deeper
+      understanding of the concepts related to coding both the front end and the
+      backend of a website. <br />
+      <br />
+      <a href="https://github.com/DwayneLaughlin" target="blank">
+        Github
+      </a>
+      <br />
+      <a href="https://docs.google.com/document/d/1REyvwUYHOvVS-X-QyyuYz1fH3P4GNd2Qh81Mvo2SBPk/edit?usp=sharing" target="blank">Resume</a>
+      <br />
+      <a href="https://www.linkedin.com/in/dwayne-laughlin-74982118b/" target="blank">
+        LinkedIn
+      </a>
+    </p>
+  );
 }
 
-export default Home
+export default Home;
