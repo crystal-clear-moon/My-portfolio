5e87b43c5dcaee34cf070f56d8e5484477d79200
DwayneL <dr2@ameritech.net>
2020-02-23 22:38:13 -0600

    added more projects to portfolio page and added picture to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,51 @@
 import React from "react";
+import me from "../assets/me.jpg";
 
 function Home() {
   return (
-    <p class="col-md-8">
-      Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in the
-      neighborhood whre I grew up on the Westside. I attended Loyola University
-      Chicago, graduating in summer of 2011 with a BA in Music. Since graduation
-      I have spent time working as a music teacher, performing pianist, composer
-      and producer. For the last 2 years I have also been the current lead for
-      the account management/collections team for zZounds.com, a leading company
-      in the instrument retail business. Part of my day to day job is
-      overlooking some of the coding of our internal wiki page and doing some
-      light coding in HTML.
-      <br />
-      <br />
-      I am currently enrolled in the Coding Bootcamp at Northwestern University
-      with a scheduled completion date of March 2020. I hope to gain a deeper
-      understanding of the concepts related to coding both the front end and the
-      backend of a website. <br />
-      <br />
-      <a href="https://github.com/DwayneLaughlin" target="blank">
-        Github
-      </a>
-      <br />
-      <a href="https://docs.google.com/document/d/1REyvwUYHOvVS-X-QyyuYz1fH3P4GNd2Qh81Mvo2SBPk/edit?usp=sharing" target="blank">Resume</a>
-      <br />
-      <a href="https://www.linkedin.com/in/dwayne-laughlin-74982118b/" target="blank">
-        LinkedIn
-      </a>
-    </p>
+    <div>
+      <p class="col-md-8">
+        Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in
+        the neighborhood whre I grew up on the Westside. I attended Loyola
+        University Chicago, graduating in summer of 2011 with a BA in Music.
+        Since graduation I have spent time working as a music teacher,
+        performing pianist, composer and producer. For the last 2 years I have
+        also been the current lead for the account management/collections team
+        for zZounds.com, a leading company in the instrument retail business.
+        Part of my day to day job is overlooking some of the coding of our
+        internal wiki page and doing some light coding in HTML.
+        <br />
+        <br />
+        I am currently enrolled in the Coding Bootcamp at Northwestern
+        University with a scheduled completion date of March 2020. I hope to
+        gain a deeper understanding of the concepts related to coding both the
+        front end and the backend of a website. <br />
+        <br />
+      </p>
+
+      
+      <div className="has-text-centered">
+        <img src={me} alt="Dwayne"></img>
+        <br></br>
+        <a href="https://github.com/DwayneLaughlin" target="blank">
+          Github
+        </a>
+        <br />
+        <a
+          href="https://docs.google.com/document/d/1REyvwUYHOvVS-X-QyyuYz1fH3P4GNd2Qh81Mvo2SBPk/edit?usp=sharing"
+          target="blank"
+        >
+          Resume
+        </a>
+        <br />
+        <a
+          href="https://www.linkedin.com/in/dwayne-laughlin-74982118b/"
+          target="blank"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </div>
   );
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,14 @@
 import React from "react";
 import tripbuddy from "../assets/tripbuddy.jpg";
-import password from "../assets/pwgen.png"
+import password from "../assets/pwgen.png";
+import notes from "../assets/notetaker.png";
+import profgen from "../assets/ProfileGenerator.png"
+import recipic from "../assets/recipic.png"
+import dayplan from "../assets/dayplan.png"
 
 function Portfolio() {
   return (
+      <div>
     <div className="columns">
       {/* tripbuddy */}
       <div className="column">
@@ -36,7 +41,76 @@ function Portfolio() {
           </a>
         </div>
     </div>
+
+    {/* Note Taker */}
+    <div className="column">
+        <a
+          href="https://rocky-stream-46867.herokuapp.com/"
+          target="blank"
+        >
+          <img src={notes} alt="Note Taker"></img>
+        </a>
+        <h1 className="has-text-centered">Note Taker</h1>
+        <div className="has-text-centered">
+          <a href="https://github.com/DwayneLaughlin/note_taker" target="blank">
+            GitHub Repository
+          </a>
+        </div>
     </div>
+</div>
+
+<div className="columns">
+
+{/* Profile Generator */}
+<div className="column">
+  <a 
+    href="https://drive.google.com/file/d/1M92mnwpmKBEKtnELk8g49062Fli68UqV/view"
+    target="blank"
+  >
+    <img src={profgen} alt="Proflie Generator"></img>
+  </a>
+  <h1 className="has-text-centered">Profile Generator</h1>
+  <div className="has-text-centered">
+    <a href="https://github.com/DwayneLaughlin/profile_generator" target="blank">
+      GitHub Repository
+    </a>
+  </div>
+</div>
+
+{/* Recipal */}
+<div className="column">
+  <a
+    href="https://shellbellquinn.github.io/ReciPal"
+    target="blank"
+  >
+    <img src={recipic} alt="ReciPal"></img>
+  </a>
+  <h1 className="has-text-centered">ReciPal</h1>
+  <div className="has-text-centered">
+    <a href="https://github.com/shellbellquinn/ReciPal" target="blank">
+      GitHub Repository
+    </a>
+  </div>
+</div>
+
+{/* Day Planner*/}
+<div className="column">
+  <a
+    href="https://dwaynelaughlin.github.io/day_planner/"
+    target="blank"
+  >
+    <img src={dayplan} alt="Day Planner"></img>
+  </a>
+  <h1 className="has-text-centered">Day Planner</h1>
+  <div className="has-text-centered">
+    <a href="https://github.com/DwayneLaughlin/day_planner" target="blank">
+      GitHub Repository
+    </a>
+  </div>
+</div>
+
+</div>
+</div>
   );
 }
 
