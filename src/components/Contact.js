55946b5913c849365736473abcd318ddb3a921e5
DwayneL <dr2@ameritech.net>
2020-02-24 18:04:05 -0600

    added form to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
-import React from "react"
+import React from "react";
 
-function Contact (){
-    return(
-        <h1>Contact goes here</h1>
-    )
+function Contact() {
+  return (
+    <div>
+      <div className="field">
+        <label className="label">Name</label>
+        <div className="control">
+          <input type="text" name="Name" value="" />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control">
+          <input type="text" name="Email" value="" />
+        </div>
+      </div>
+
+      <div className="field">
+          <label className="label">Message</label>
+          <div className="control">
+            <textarea
+              type="textarea"
+              name="Message"
+              rows="10"
+              placeholder="Please Enter a Height"
+            />
+          </div>
+        </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
