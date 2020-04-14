73edf41066be61862f330893aaa6113c8b5e0930
DwayneL <dr2@ameritech.net>
2020-04-14 18:59:42 -0500

    updated formatting on profile page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import me from "../assets/me.jpg";
-import resume from "../assets/Resume2019.pdf"
+import resume from "../assets/Resume2019.pdf";
 
 function Home() {
   return (
     <div>
-      <p class="col-md-8">
+    <div className="columns">
+      <div className="column is-1"></div>
+      <p className="column">
         Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in
         the neighborhood whre I grew up on the Westside. I attended Loyola
         University Chicago, graduating in summer of 2011 with a BA in Music.
@@ -14,28 +16,25 @@ function Home() {
         also been the current lead for the account management/collections team
         for zZounds.com, a leading company in the instrument retail business.
         Part of my day to day job is overlooking some of the coding of our
-        internal wiki page and doing some light coding in HTML.
+        internal wiki page and doing some light coding in HTML as well as using
+        Ecometry for managing our payment sytsem and doing data entry.
         <br />
         <br />
-        I am currently enrolled in the Coding Bootcamp at Northwestern
-        University with a scheduled completion date of March 2020. I hope to
-        gain a deeper understanding of the concepts related to coding both the
-        front end and the backend of a website. <br />
+        I completed the Northwestern Coding Bootcamp in March 2020 and have
+        experience with HTML, CSS, Bootstrap, Bulma, Node, Javascript, SQL, Mongo, and React. <br />
         <br />
       </p>
+      <div className="column is-1"></div>
+      </div>
 
-      
-      <div className="has-text-centered">
+      <div className="column is-full has-text-centered">
         <img src={me} alt="Dwayne"></img>
         <br></br>
         <a href="https://github.com/DwayneLaughlin" target="blank">
           Github
         </a>
         <br />
-        <a
-          href={resume}
-          target="blank"
-        >
+        <a href={resume} target="blank">
           Resume
         </a>
         <br />
