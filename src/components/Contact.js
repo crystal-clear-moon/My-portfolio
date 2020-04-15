96f6293e770a3e7283a81235567cd793da06d328
DwayneL <dr2@ameritech.net>
2020-04-14 19:36:22 -0500

    updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,32 +3,20 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <div className="field">
-        <label className="label">Name</label>
-        <div className="control">
-          <input type="text" name="Name" value="" />
-        </div>
-      </div>
+      <div className="has-text-centered has-text-weight-bold is-size-1">Contact</div>
+      <div className="columns">
+        <div className="column is-1"></div>
+        
+        <div className="column has-text-centered is-size-4">
+          Phone: 773.263.2678<br />
+          Email: dwaynelaughlin@gmail.com
 
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control">
-          <input type="text" name="Email" value="" />
         </div>
-      </div>
 
-      <div className="field">
-          <label className="label">Message</label>
-          <div className="control">
-            <textarea
-              type="textarea"
-              name="Message"
-              rows="10"
-              placeholder="Please Enter a Height"
-            />
-          </div>
-        </div>
+        <div className="column is-1"></div>
+      </div>
     </div>
+      
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,26 +5,27 @@ import resume from "../assets/Resume2019.pdf";
 function Home() {
   return (
     <div>
-    <div className="columns">
-      <div className="column is-1"></div>
-      <p className="column">
-        Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in
-        the neighborhood whre I grew up on the Westside. I attended Loyola
-        University Chicago, graduating in summer of 2011 with a BA in Music.
-        Since graduation I have spent time working as a music teacher,
-        performing pianist, composer and producer. For the last 2 years I have
-        also been the current lead for the account management/collections team
-        for zZounds.com, a leading company in the instrument retail business.
-        Part of my day to day job is overlooking some of the coding of our
-        internal wiki page and doing some light coding in HTML as well as using
-        Ecometry for managing our payment sytsem and doing data entry.
-        <br />
-        <br />
-        I completed the Northwestern Coding Bootcamp in March 2020 and have
-        experience with HTML, CSS, Bootstrap, Bulma, Node, Javascript, SQL, Mongo, and React. <br />
-        <br />
-      </p>
-      <div className="column is-1"></div>
+      <div className="columns">
+        <div className="column is-1"></div>
+        <p className="column">
+          Hi! I'm Dwayne Laughlin. I'm a lifelong Chicagoan currently living in
+          the neighborhood whre I grew up on the Westside. I attended Loyola
+          University Chicago, graduating in summer of 2011 with a BA in Music.
+          Since graduation I have spent time working as a music teacher,
+          performing pianist, composer and producer. For the last 2 years I have
+          also been the current lead for the account management/collections team
+          for zZounds.com, a leading company in the instrument retail business.
+          Part of my day to day job is overlooking some of the coding of our
+          internal wiki page and doing some light coding in HTML as well as
+          using Ecometry for managing our payment sytsem and doing data entry.
+          <br />
+          <br />
+          I completed the Northwestern Coding Bootcamp in March 2020 and have
+          experience with HTML, CSS, Bootstrap, Bulma, Node, Javascript, SQL,
+          Mongo, and React. <br />
+          <br />
+        </p>
+        <div className="column is-1"></div>å
       </div>
 
       <div className="column is-full has-text-centered">
